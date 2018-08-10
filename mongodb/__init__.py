3d9df3b8f13e1312838ae02a873ef68f6477f5ff
MaLei <826908021@qq.com>
2018-08-10 18:16:32 +0800

    mongodb测试

--- mongodb/__init__.py
+++ mongodb/__init__.py
@@ -9,15 +9,15 @@ db = client.python
 collection=db.python
 
 # 插入数据
-# python = {
-#     'id': '20170101',
-#     'name': 'Jordan',
-#     'age': 20,
-#     'gender': 'male'
-# }
-# result=collection.insert_one(python)
-# print(result)
-# print(result.inserted_id)
+python = {'content': '对于经济学、商科的学生来说，使用数理统计的方法做文章的套路可能理所当然：观察到一个现象，提出因果关系假说， 收集大量合适的数据跑回归，得出结论，再对结论做robust检验，基本上这篇文章就能立住了。但是遗憾的是，很多时候，这个套路在历史研究当中是…',
+ 'name': '赫连镜繇',
+ 'num': '190',
+ 'que_url': 'https://www.zhihu.com/question/281446079/answer/422900276',
+ 'question': '历史学专业是如何分析数据的呢？'}
+
+result=collection.insert_one(python)
+print(result)
+print(result.inserted_id)
 
 # 查询单条数据
 # result=collection.find_one({'_id': ObjectId('5b177a5df728ae9074b49a67')})
@@ -66,6 +66,6 @@ collection=db.python
 # result=collection.delete_one({'name':'Jordan'})
 # print(result)
 # print(result.deleted_count)
-result=collection.delete_many({'name':'Jordan'})
+# result=collection.delete_many({'name':'Jordan'})
 print(result)
-print(result.deleted_count)
+# print(result.deleted_count)
