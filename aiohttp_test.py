69e212ced1bbeef8f6ec16e1f13fa31c8a1d1535
Malei666 <826908021@qq.com>
2019-02-21 21:30:21 +0800

    aiohttp测试

--- aiohttp_test.py
+++ aiohttp_test.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
-import aiohttp
+import requests,time,aiohttp,asyncio
 
-async with aiohttp.ClientSession() as session:
-    async with session.get('www.baidu.com')as resp:
-        print(resp.status)
-        print(await resp.text)
+URL = 'https://morvanzhou.github.io/'
+
+
+def normal():
+    for i in range(2):
+        r = requests.get(URL)
+        url = r.url
+        print(url)
+
+t1 = time.time()
+normal()
+print("Normal total time:", time.time()-t1)
+
+async def job(session):
+    response = await session.get(URL)       # 等待并切换
+    return str(response.url)
+
+
+async def main(loop):
+    async with aiohttp.ClientSession() as session:      # 官网推荐建立 Session 的形式
+        tasks = [loop.create_task(job(session)) for _ in range(2)]
+        finished, unfinished = await asyncio.wait(tasks)
+        all_results = [r.result() for r in finished]    # 获取所有结果
+        print(all_results)
+
+t1 = time.time()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop))
+loop.close()
+print("Async total time:", time.time() - t1)
