1001769569e5d3e1199890cf5e5639419a9ef486
MaLei <826908021@qq.com>
2019-03-19 18:04:05 +0800

    celery测试

---
+++ Celery/__init__.py
@@ -0,0 +1,6 @@
+#!/home/zkfr/.local/share/virtualenvs/xf-5EfV3Nly/bin/python
+#-*- coding:utf-8 -*-
+# @author : MaLei 
+# @datetime : 2019-03-19 17:00
+# @file : __init__.py.py
+# @software : PyCharm
