61ae89e99ebc5ada53c1061445efb342416a28df
Malei666 <826908021@qq.com>
2019-07-04 22:17:14 +0800

    note

--- data_struct_test.py
+++ data_struct_test.py
@@ -8,21 +8,39 @@
 # for key, value in pairs:
 #     d[key].append(value)
 
-from collections import deque
-class linehistory:
-    def __init__(self, lines, histlen=3):
-            self.lines = lines
-            self.history = deque(maxlen=histlen)
-    def __iter__(self):
-        for lineno, line in enumerate(self.lines, 1):
-            self.history.append((lineno, line))
-            yield line
-    def clear(self):
-        self.history.clear()
+import os,fnmatch,gzip,bz2,re
 
-with open('somefile.txt') as f:
-    lines = linehistory(f)
+#查找所有符合条件的文件
+def gen_find(filepat, top):
+    for path, dirlist, filelist in os.walk(top):
+        for name in fnmatch.filter(filelist, filepat):
+            yield os.path.join(path,name)
+def gen_opener(filenames):
+    for filename in filenames:
+        if filename.endswith('.gz'):
+            f = gzip.open(filename, 'rt')
+        elif filename.endswith('.bz2'): f = bz2.open(filename, 'rt')
+        else:
+            f = open(filename, 'rt')
+        yield f
+        f.close()
+def gen_concatenate(iterators):
+    for it in iterators:
+        yield from it
+def gen_grep(pattern, lines):
+    pat = re.compile(pattern)
     for line in lines:
-        if 'python' in line:
-            for lineno, hline in lines.history:
-                print('{}:{}'.format(lineno, hline), end='')
+        if pat.search(line):
+            yield line
+
+lognames = gen_find('access-log*', 'www')
+files = gen_opener(lognames)
+lines = gen_concatenate(files)
+pylines = gen_grep('(?i)python', lines)
+#查找包含单词 python 的所有日志行
+for line in pylines:
+    print(line)
+#计算出传输的字节数并计算其总和
+bytecolumn = (line.rsplit(None,1)[1] for line in pylines)
+bytes = (int(x) for x in bytecolumn if x != '-')
+print('Total', sum(bytes))
