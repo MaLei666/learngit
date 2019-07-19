20aa3e8644b1fad936f643a3892eaca760236b63
Malei666 <826908021@qq.com>
2019-07-19 21:52:42 +0800

    note

--- data_struct_test.py
+++ data_struct_test.py
@@ -8,3 +8,23 @@
 # for key, value in pairs:
 #     d[key].append(value)
 
+
+from functools import partial
+def spam(a, b, c, d):
+    print(a, b, c, d)
+
+s1 = partial(spam, 1)
+s2 = partial(spam, d=42)
+s3 = partial(spam, 1, 2, d=42)
+
+
+
+
+
+
+
+
+
+
+
+
