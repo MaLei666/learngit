22f7d926aefa83442f526f1790887724fb73ff16
MaLei <826908021@qq.com>
2019-08-02 17:58:02 +0800

    note

--- class_and_object.py
+++ class_and_object.py
@@ -54,31 +54,195 @@
 #     self.month = month
 #     self.day = day
 
-class A:
-    def __init__(self):
-        self._internal = 0 # An internal attribute
-        self.public = 1 # A public attribute
-    def public_method(self):
-        pass
-    def _internal_method(self):
-        pass
+# class A:
+#     def __init__(self):
+#         self._internal = 0 # An internal attribute
+#         self.public = 1 # A public attribute
+#     def public_method(self):
+#         pass
+#     def _internal_method(self):
+#         pass
+#
+# class B:
+#     def __init__(self):
+#         self.__private = 0
+#     def __private_method(self):
+#         pass
+#     def public_method(self):
+#         pass
+#         self.__private_method()
+#
+# class C(B):
+#     def __init__(self):
+#         super().__init__()
+#         self.__private = 1 # Does not override B.__private
+#         # Does not override B.__private_method()
+#     def __private_method(self):
+#         pass
 
-class B:
-    def __init__(self):
-        self.__private = 0
-    def __private_method(self):
-        pass
-    def public_method(self):
-        pass
-        self.__private_method()
+# class Person:
+#     def __init__(self, first_name):
+#         self.first_name = first_name
+#     # Getter function
+#     @property
+#     def first_name(self):
+#         return self._first_name
+#     # Setter function
+#     @first_name.setter
+#     def first_name(self, value):
+#         if not isinstance(value, str):
+#             raise TypeError('Expected a string')
+#         self._first_name = value
+#     # Deleter function (optional)
+#     @first_name.deleter
+#     def first_name(self):
+#         raise AttributeError("Can't delete attribute")
+#
+# a=Person(1)
+# b=a.first_name
+#
+# print(b)
 
-class C(B):
-    def __init__(self):
-        super().__init__()
-        self.__private = 1 # Does not override B.__private
-        # Does not override B.__private_method()
-    def __private_method(self):
-        pass
+# class Person:
+#     def __init__(self, first_name):
+#         self.set_first_name(first_name)
+#     # Getter function
+#     def get_first_name(self):
+#         return self._first_name
+#     # Setter function
+#     def set_first_name(self, value):
+#         if not isinstance(value, str):
+#             raise TypeError('Expected a string')
+#         self._first_name = value
+#     # Deleter function (optional)
+#     def del_first_name(self):
+#         raise AttributeError("Can't delete attribute")
+#     # Make a property from existing get/set methods
+#     name = property(get_first_name, set_first_name, del_first_name)
+
+
+# class Proxy:
+#     def __init__(self, obj):
+#         self._obj = obj
+#     # Delegate attribute lookup to internal obj
+#     def __getattr__(self, name):
+#         return getattr(self._obj, name)
+#     # Delegate attribute assignment
+#     def __setattr__(self, name, value):
+#         if name.startswith('_'):
+#             super().__setattr__(name, value) # Call original __setattr__
+#         else:
+#             setattr(self._obj, name, value)
+#
+# class A:
+#     def __init__(self):
+#         self.x = 0
+# class B(A):
+#     def __init__(self):
+#         super().__init__()
+#         self.y = 1
+
+
+
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+#     # Getter function
+#     @property
+#     def name(self):
+#         return self._name
+#     # Setter function
+#     @name.setter
+#     def name(self, value):
+#         if not isinstance(value, str):
+#             raise TypeError('Expected a string')
+#         self._name = value
+#     # Deleter function
+#     @name.deleter
+#     def name(self):
+#         raise AttributeError("Can't delete attribute")
+#
+# class SubPerson(Person):
+#     @property
+#     def name(self):
+#         print('Getting name')
+#         return super().name
+#     @name.setter
+#     def name(self, value):
+#         print('Setting name to', value)
+#         super(SubPerson, SubPerson).name.__set__(self, value)
+#     @name.deleter
+#     def name(self):
+#         print('Deleting name')
+#         super(SubPerson, SubPerson).name.__delete__(self)
+#
+#
+# class SubPerson(Person):
+#     @Person.name.getter
+#     def name(self):
+#         print('Getting name')
+#         return super().name
+#
+# class SubPerson(Person):
+#     @Person.name.setter
+#     def name(self, value):
+#         print('Setting name to', value)
+#         super(SubPerson, SubPerson).name.__set__(self, value)
+
+
+class Integer:
+    def __init__(self, name):
+        self.name = name
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.name]
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected an int')
+        instance.__dict__[self.name] = value
+    def __delete__(self, instance):
+        del instance.__dict__[self.name]
+
+class Point:
+    x = Integer('x')
+    y = Integer('y')
+
+def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
