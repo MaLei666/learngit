5653c4d42ab488660b259aae676a0385bcfa076b
Malei666 <826908021@qq.com>
2020-08-06 21:39:34 +0800

    update

--- data_struct/list_test.py
+++ data_struct/list_test.py
@@ -3,3 +3,43 @@
 # @datetime : 2020/7/28 9:05 下午
 # @file : list_test.py
 # @software : PyCharm
+
+# colors = ['black', 'white']
+# sizes = ['S', 'M', 'L']
+# #列表表达式
+# a=[(i,j) for i in sizes for j in colors]
+# print(type(a))
+# print(a)
+#
+# #生成器表达式
+# a=((i,j) for i in sizess for j in colors)
+# print(type(a))
+# print(tuple(a))
+
+# from types import MappingProxyType
+# a={1:2}
+# d_pro=MappingProxyType(a)
+# print(d_pro)
+# # d_pro[2]=3
+#
+# a[2]=3
+# print(d_pro)
+
+# a=set([1,2,1])
+# print(a)
+# a.update([1])
+# print(a)
+# a.add(3)
+# print(a)
+# print(a&{1,3})
+# print({1,3}&a)
+# a=frozenset([1,2,3])
+# print(a)
+# b=a.difference([1,2])
+# print(b)
+
+# print(hash(1),hash(1.0))
+# print(hash(1.000001),hash(1.000002))
+
+fro
+
