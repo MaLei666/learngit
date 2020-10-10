a291455b620e9b3c71ff0ba2de56ef7c81ab5e5f
malei <826908021@qq.com>
2020-10-10 09:45:26 +0800

    update

--- data_struct/data_struct_test.py
+++ data_struct/data_struct_test.py
@@ -105,17 +105,67 @@
 #     bisect.insort(a,score)
 # print(a)
 
-from collections import deque
-dq=deque(range(10),maxlen=10)
-dq.rotate(-5)
-print(dq)
-dq.rotate(5)
-print(dq)
-dq.append('a')
-print(dq)
-dq.extend(['a'])
-print(dq)
-dq.extendleft(('a','b'))
-print(dq)
+# from collections import deque
+# dq=deque(range(10),maxlen=10)
+# dq.rotate(-5)
+# print(dq)
+# dq.rotate(5)
+# print(dq)
 # dq.append('a')
 # print(dq)
+# dq.extend(['a'])
+# print(dq)
+# dq.extendleft(('a','b'))
+# print(dq)
+# dq.append('a')
+# print(dq)
+#
+# import re
+# re_numbers_str = re.compile(r'\d+')
+# re_words_str = re.compile(r'\w+')
+# re_numbers_bytes = re.compile(rb'\d+')
+# re_words_bytes = re.compile(rb'\w+')
+# text_str = ("Ramanujan saw \u0be7\u0bed\u0be8\u0bef"
+#             " as 1729 = 1³ + 12³ = 9³ + 10³.")
+# text_bytes = text_str.encode('utf_8')
+# print('Text', repr(text_str), sep='\n ')
+# print('Numbers')
+# print(' str :', re_numbers_str.findall(text_str))
+# print(' bytes:', re_numbers_bytes.findall(text_bytes))
+# print('Words')
+# print(' str :', re_words_str.findall(text_str))
+# print(' bytes:', re_words_bytes.findall(text_bytes))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
