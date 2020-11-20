42a4dfb7c8892ed0e6b8afc1ad83544c6df62598
malei <826908021@qq.com>
2020-11-20 14:46:18 +0800

    update

--- Decorator/装饰器.py
+++ Decorator/装饰器.py
@@ -4,18 +4,44 @@
 # @file : 装饰器.py
 # @software : PyCharm
 
-def deco(func=None):
-    def new():
-        print('failed')
-    if func:
-        print('succeed!')
-        return func
-    else:
-        print('run new()')
-        return new
+# def deco(func=None):
+#     def new():
+#         print('failed')
+#     if func:
+#         print('succeed!')
+#         return func
+#     else:
+#         print('run new()')
+#         return new
+#
+# @deco
+# def test_func():
+#     print('this is a test function!')
+#
+# test_func()
 
-@deco
-def test_func():
-    print('this is a test function!')
 
-test_func()
+import time
+def clock(func):
+    def son(*args):
+        t0 = time.perf_counter()
+        res = func(*args)
+        els=time.perf_counter()-t0
+        name=func.__name__
+        args_str=','.join(repr(arg) for arg in args)
+        print('[%0.8fs] %s(%s) -> %r' % (els, name, args_str, res))
+        return res
+    return son
+
+@clock
+def test(seconds):
+    time.sleep(seconds)
+
+@clock
+def test2(n):
+    return 1 if n<2 else n*test2(n-1)
+
+print('*' * 40, 'Calling snooze(.123)')
+test(.123)
+print('*' * 40, 'Calling factorial(6)')
+print('6! =', test2(6))
--- Decorator/闭包.py
+++ Decorator/闭包.py
@@ -4,17 +4,33 @@
 # @file : 闭包.py
 # @software : PyCharm
 
-def test():
-    test_list=[]
-    def test_son(value):
-        test_list.append(value)
-        total=sum(test_list)
-        print(test_list)
-        return total/len(test_list)
-    return test_son
+# def test():
+#     test_list=[]
+#     def test_son(value):
+#         test_list.append(value)
+#         total=sum(test_list)
+#         print(test_list)
+#         return total/len(test_list)
+#     return test_son
+#
 
-a=test()
-print(a(10))
-print(a(13))
-print(a(22))
-print(a(21))
+
+# def test():
+#     count=0
+#     total=0
+#     def test1(value):
+#         #当 count 是数字或任何不可变类型时，我们在 test1 的定义体中为 count 赋值了，这会把 count 变成局部变量。
+#         # count += 1
+#         # total += value
+#         # return total / count
+#         nonlocal count,total
+#         count += 1
+#         total += value
+#         return total / count
+#     return test1
+#
+# a=test()
+# print(a(10))
+# print(a(13))
+# print(a(22))
+# print(a(21))
