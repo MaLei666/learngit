aa6451468e0bc594bd98a90e61a9bcd9aff93ab4
malei <826908021@qq.com>
2020-12-23 16:53:53 +0800

    迭代器和生成器

--- Iterator_and_Generator/generator_expression.py
+++ Iterator_and_Generator/generator_expression.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # @author : MaLei 
 # @datetime : 2020/12/21 16:37
 # @file : generator_expression.py
@@ -7,11 +7,12 @@
 import reprlib
 import re
 
-
 '''
-生成器表达式是语法糖：完全可以替换成生成器函数，不过有时使用生 成器表达式更便利
-'''
+生成器表达式是语法糖：完全可以替换成生成器函数，不过有时使用生成器表达式更便利
 
+生成器表达式是创建生成器的简洁句法，这样无需先定义函数再调用
+不过，生成器函数灵活得多，可以使用多个语句实现复杂的逻辑，也可以作为协程使用
+'''
 
 class Sentence:
     def __init__(self, text):
@@ -26,9 +27,50 @@ class Sentence:
         # for i in self.RE_WORD.finditer(self.text):
         #     yield i.group()
         return (i.group() for i in self.RE_WORD.finditer(self.text))
+#
+#
+# a=Sentence('a c v f ff')
+# print(type(a))
+# for i in a:
+#     print(i)
+#
 
 
-a=Sentence('a c v f ff')
-print(type(a))
-for i in a:
+'''
+典型的迭代器模式作用很简单：遍历数据结构。
+不过，即便不是从集合中获取元素，而是获取序列中即时生成的下一个值时，也用得到这种基于方法的标准接口。
+'''
+
+
+class ArithmeticProgression:
+    def __init__(self, begin, step, end=None):
+        self.begin = begin
+        self.step = step
+        self.end = end
+
+    def __iter__(self):
+        # 把 self.begin 赋值给 result，不过会先强制转换成前面的加法算式得到的类型
+        print(type(self.begin + self.step))
+        result = type(self.begin + self.step)(self.begin)
+        # 如果 self.end 属性的值是 None，那么forever的值是True
+        forever = self.end is None
+        print(forever)
+        index = 0
+        while forever or result < self.end:
+            yield result
+            index += 1
+            result = self.begin + self.step * index
+
+
+# ap = ArithmeticProgression(0, 1, 3)
+# print(list(ap))
+
+
+import itertools
+gen=itertools.count(1,2)
+# for i in range(10):
+#     print(next(gen))
+
+gen2=itertools.takewhile(lambda n:n<10,gen)
+for i in gen2:
     print(i)
---
+++ Iterator_and_Generator/标准库中的生成器函数.py
@@ -0,0 +1,110 @@
+# -*- coding:utf-8 -*-
+# @author : MaLei 
+# @datetime : 2020/12/22 16:35
+# @file : 标准库中的生成器函数.py
+# @software : PyCharm
+
+'''
+itertools:
+'''
+
+'''
+用于过滤的生成器函数:
+
+1. compress(it,selector_it)
+并行处理两个可迭代的对象；如果selector_it中的元素是真值，产出it中对应的元素
+'''
+
+import itertools
+
+# a = [1, 2, 3, 4, 5]
+# b = [1, None, 0, [], {}]
+# for i in itertools.compress(a, b):
+#     print(i)
+#
+# a = 'test_str'
+# b = [1, 0, 1, 0, 0, 1, 1, 1]
+# for i in itertools.compress(a, b):
+#     print(i)
+
+'''
+2. dropwhile(predicate,it)
+处理it，跳过predicate的计算结果为真值的元素，然后产出剩下的各个元素（不再进一步检查）
+'''
+# for i in itertools.dropwhile(lambda n: n.lower() in 'test', 'test_str'):
+#     print(i)
+
+'''
+3. filter(predicate,it)  内置函数
+把it中的各个元素传给predicate，
+如果predicate(item)返回真值，那么产出对应的元素；
+如果predicate是None，那么只产出真值元素
+'''
+# print(list(filter(lambda n:n in 'teS','test_str_S')))
+
+'''
+4. filterfalse(predicate,it)
+与filter函数的作用类似，不过predicate的逻辑是相反的：
+predicate返回假值时产出对应的元素
+'''
+# print(list(itertools.filterfalse(lambda n:n in 'teS','test_str')))
+
+'''
+
+5. takewhile(predicate,it)
+predicate返回真值时产出对应的元素，然后立即停止，不再继续检查
+'''
+# print(list(itertools.takewhile(lambda n: n in 'ter', 'test_str')))
+
+'''
+6. islice(it,stop)或islice(it,start,stop,step=1)
+产出it的切片，作用类似于s[:stop]或s[start:stop:step]，
+不过it可以是任何可迭代的对象，而且这个函数实现的是惰性操作
+'''
+# print(list(itertools.islice('abcdefg',0,10,2)))
+# print(list(itertools.islice([1,2,3,4,5],0,10,2)))
+
+'''
+用于映射的生成器函数
+itertools
+
+accumulate(it,[func])
+产出累积的总和；
+如果提供了func，那么把前两个元素传给它，然后把计算结果和下一个元素传给它，以此类推，最后产出结果
+'''
+# print(list(itertools.accumulate([1,2,3,9],lambda n,t:n+t)))
+# print(list(itertools.accumulate([1,2,3,9])))
+# print(list(itertools.accumulate([1,2,3,9],min)))
+# print(list(itertools.accumulate([1,2,3,9],max)))
+# import operator
+# print(list(itertools.accumulate([1,2,3,9],operator.mul)))
+
+'''
+（内置）enumerate(iterable,start=0)
+产出由两个元素组成的元组，结构是(index,item)，
+其中index从start开始计数，item则从iterable中获取
+'''
+# print(list(enumerate([1,2,3])))
+
+'''
+（内置）map(func,it1,[it2,...,itN])
+把it中的各个元素传给func，产出结果；
+如果传入N个可迭代的对象，那么func必须能接受N个参数，
+而且要并行处理各个可迭代的对象
+'''
+# print(list(map(lambda n:n*n,[1,2,3])))
+# print(list(map(lambda n:n+'——test','abcd')))
+
+'''
+starmap(func,it)
+把it中的各个元素传给func，产出结果；
+输入的可迭代对象应该产出可迭代的元素iit，
+然后以func(*iit)这种形式调用func
+'''
+# print(list(itertools.starmap(lambda n:n+'_123','adecsedwfew')))
+
+'''
+用于合并的生成器函数
+chain(it1, ..., itN) 先产出 it1 中的所有元素，然后产出 it2 中的 所有元素，以此类推，无缝连接在一起 itertools chain.from_iterable(it) 产出 it 生成的各个可迭代对象中的元素，一个 接一个，无缝连接在一起；it 应该产出可迭代 的元素，例如可迭代的对象列表 itertools product(it1, ..., itN, repeat=1) 计算笛卡儿积：从输入的各个可迭代对象中获 取元素，合并成由 N 个元素组成的元组，与嵌 套的 for 循环效果一样；repeat 指明重复处理 多少次输入的可迭代对象 （内置） zip(it1, ..., itN) 并行从输入的各个可迭代对象中获取元素，产 出由 N 个元素组成的元组，只要有一个可迭代 的对象到头了，就默默地停止
+itertools zip_longest(it1, ..., itN, fillvalue=None) 并行从输入的各个可迭代对象中获取元素，产 出由 N 个元素组成的元组，等到最长的可迭代 对象到头后才停止，空缺的值使用 fillvalue 填充
+'''
